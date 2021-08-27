ebe8b10807279dc3599a75eaabfd7fb0a96ab0f3
Christian Prinz <prinzach@b-tu.de>
2021-08-27 16:47:05 +0200

    formatted with prettier rules

--- src/Decorators.ts
+++ src/Decorators.ts
@@ -11,56 +11,66 @@ import { LogLevelType, LoxerOptions } from './types';
  * @returns a class decorator
  */
 export function initLoxer(options: LoxerOptions) {
-    Loxer.init(options);
+  Loxer.init(options);
 }
 
 /**
  * The Options for the `@trace(options: TracerOptions | string)` decorator
  */
 interface TraceOptions {
-    /** the corresponding key of a `LoxerModule` provided in the `LoxerOptions.modules` for the `Loxer.init(options: LoxerOptions)` initialization */
-    moduleId?: string;
-    /** the level of the log. defaults to `1` */
-    level?: LogLevelType;
-    /** which messages should be highlighted */
-    highlight?: 'open' | 'close' | 'all';
-    /** how should the opening message be styled. for example if `MyServiceClass.myFunction(a: number, b: string)`
-     * is called with `myFunction(3, "test")`:
-     * - `'functionName'`: prints `"myFunction()"`
-     * - `'className.functionName'`: prints `"MyService.myFunction()"` (the postfix `Class` will be erased though it
-     *   is clear that it's a class, when a method is decorated)
-     * - `'types'`: prints `"myFunction(number, string)"` (with the types of the actual arguments)
-     * - `'args'`: prints `"myFunction(3, "test")"` (with the actual arguments)
-     * - `(args: any[]) => string)` is a callback which provides the actual arguments.
-     * - defaults to `'functionName'`
-     */
-    openMessage?: ((args: any[]) => string) | 'functionName' | 'className.functionName' | 'types' | 'args';
-    /** how should the opening message be styled. for example if `MyServiceClass.myFunction(a: number, b: string)`
-     * returns `{val: "test", count: 5}`:
-     * - `'functionName'`: prints `"myFunction done"`
-     * - `'className.functionName'`: prints `"MyService.myFunction done"` (the postfix `Class` will be erased though it
-     *   is clear that it's a class, when a method is decorated)
-     * - `'result'`: prints `"myFunction done. returns: {val: "test", count: 5}"`
-     * - `'prettyResult'`: prints:
-     *   ```string
-     *   myFunction done. returns:
-     *   {
-     *       val: "test",
-     *       count: 5
-     *   }
-     *   ```
-     * - `(result?: any) => string)` is a callback which provides the actual result.
-     * - defaults to `'functionName'`
-     */
-    closeMessage?: ((result?: any) => string) | 'functionName' | 'className.functionName' | 'result' | 'prettyResult';
-    /** appends the arguments as `item: any` to the open log.
-     * - defaults to `false`
-     */
-    argsAsItem?: boolean;
-    /** appends the result as `item: any` to the close log.
-     * - defaults to `false`
-     */
-    resultAsItem?: boolean;
+  /** the corresponding key of a `LoxerModule` provided in the `LoxerOptions.modules` for the `Loxer.init(options: LoxerOptions)` initialization */
+  moduleId?: string;
+  /** the level of the log. defaults to `1` */
+  level?: LogLevelType;
+  /** which messages should be highlighted */
+  highlight?: 'open' | 'close' | 'all';
+  /** how should the opening message be styled. for example if `MyServiceClass.myFunction(a: number, b: string)`
+   * is called with `myFunction(3, "test")`:
+   * - `'functionName'`: prints `"myFunction()"`
+   * - `'className.functionName'`: prints `"MyService.myFunction()"` (the postfix `Class` will be erased though it
+   *   is clear that it's a class, when a method is decorated)
+   * - `'types'`: prints `"myFunction(number, string)"` (with the types of the actual arguments)
+   * - `'args'`: prints `"myFunction(3, "test")"` (with the actual arguments)
+   * - `(args: any[]) => string)` is a callback which provides the actual arguments.
+   * - defaults to `'functionName'`
+   */
+  openMessage?:
+    | ((args: any[]) => string)
+    | 'functionName'
+    | 'className.functionName'
+    | 'types'
+    | 'args';
+  /** how should the opening message be styled. for example if `MyServiceClass.myFunction(a: number, b: string)`
+   * returns `{val: "test", count: 5}`:
+   * - `'functionName'`: prints `"myFunction done"`
+   * - `'className.functionName'`: prints `"MyService.myFunction done"` (the postfix `Class` will be erased though it
+   *   is clear that it's a class, when a method is decorated)
+   * - `'result'`: prints `"myFunction done. returns: {val: "test", count: 5}"`
+   * - `'prettyResult'`: prints:
+   *   ```string
+   *   myFunction done. returns:
+   *   {
+   *       val: "test",
+   *       count: 5
+   *   }
+   *   ```
+   * - `(result?: any) => string)` is a callback which provides the actual result.
+   * - defaults to `'functionName'`
+   */
+  closeMessage?:
+    | ((result?: any) => string)
+    | 'functionName'
+    | 'className.functionName'
+    | 'result'
+    | 'prettyResult';
+  /** appends the arguments as `item: any` to the open log.
+   * - defaults to `false`
+   */
+  argsAsItem?: boolean;
+  /** appends the result as `item: any` to the close log.
+   * - defaults to `false`
+   */
+  resultAsItem?: boolean;
 }
 
 /**
@@ -71,80 +81,98 @@ interface TraceOptions {
  * @returns a Decorator for class level methods
  */
 export function trace(options?: TraceOptions | string) {
-    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-        const original = descriptor.value;
-        const className: string = target.constructor.name;
-        const fixedName = className.endsWith('Class') ? className.substr(0, className.length - 5) : className;
-        descriptor.value = async function (...args: any[]) {
-            let moduleId;
-            let o;
-            if (is(options) && typeof options === 'string') {
-                moduleId = options;
-            } else if (is(options)) {
-                o = options;
-                moduleId = o?.moduleId;
-            }
+  return function(
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const original = descriptor.value;
+    const className: string = target.constructor.name;
+    const fixedName = className.endsWith('Class')
+      ? className.substr(0, className.length - 5)
+      : className;
+    descriptor.value = async function(...args: any[]) {
+      let moduleId;
+      let o;
+      if (is(options) && typeof options === 'string') {
+        moduleId = options;
+      } else if (is(options)) {
+        o = options;
+        moduleId = o?.moduleId;
+      }
 
-            const level = o?.level ?? 1;
-            const h = o?.highlight;
+      const level = o?.level ?? 1;
+      const h = o?.highlight;
 
-            // open message
-            let openMessage = getOpenMessage(o, propertyKey, args, fixedName);
+      // open message
+      let openMessage = getOpenMessage(o, propertyKey, args, fixedName);
 
-            // open the lox
-            const item = o?.argsAsItem ? args : undefined;
-            const loxId = Loxer.h(h === 'all' || h === 'open')
-                .l(level)
-                .m(moduleId)
-                .open(openMessage, item);
+      // open the lox
+      const item = o?.argsAsItem ? args : undefined;
+      const loxId = Loxer.h(h === 'all' || h === 'open')
+        .l(level)
+        .m(moduleId)
+        .open(openMessage, item);
 
-            // call the function
-            const result = await original.call(this, ...args);
+      // call the function
+      const result = await original.call(this, ...args);
 
-            // close message
-            let closeMessage = getCloseMessage(o, propertyKey, result, fixedName);
+      // close message
+      let closeMessage = getCloseMessage(o, propertyKey, result, fixedName);
 
-            // close the lox
-            const resultItem = o?.resultAsItem ? result : undefined;
-            Loxer.h(h === 'all' || h === 'close')
-                .of(loxId)
-                .close(closeMessage, resultItem);
+      // close the lox
+      const resultItem = o?.resultAsItem ? result : undefined;
+      Loxer.h(h === 'all' || h === 'close')
+        .of(loxId)
+        .close(closeMessage, resultItem);
 
-            return result;
-        };
+      return result;
     };
+  };
 }
 
-function getOpenMessage(o: TraceOptions | undefined, propertyKey: string, args: any[], fixedName: string) {
-    const om = o?.openMessage;
-    let openMessage = propertyKey + '()';
-    if (is(om)) {
-        if (typeof om === 'function') {
-            openMessage = om(args);
-        } else if (om === 'args') {
-            openMessage = propertyKey + '(' + args.join(', ') + ')';
-        } else if (om === 'types') {
-            openMessage = propertyKey + '(' + args.map((a) => typeof a).join(', ') + ')';
-        } else if (om === 'className.functionName') {
-            openMessage = fixedName + '.' + propertyKey + '()';
-        }
+function getOpenMessage(
+  o: TraceOptions | undefined,
+  propertyKey: string,
+  args: any[],
+  fixedName: string
+) {
+  const om = o?.openMessage;
+  let openMessage = propertyKey + '()';
+  if (is(om)) {
+    if (typeof om === 'function') {
+      openMessage = om(args);
+    } else if (om === 'args') {
+      openMessage = propertyKey + '(' + args.join(', ') + ')';
+    } else if (om === 'types') {
+      openMessage =
+        propertyKey + '(' + args.map(a => typeof a).join(', ') + ')';
+    } else if (om === 'className.functionName') {
+      openMessage = fixedName + '.' + propertyKey + '()';
     }
-    return openMessage;
+  }
+  return openMessage;
 }
 
-function getCloseMessage(o: TraceOptions | undefined, propertyKey: string, result: any, fixedName: string) {
-    const cm = o?.closeMessage;
-    let closeMessage = propertyKey + ' done';
-    if (is(cm)) {
-        if (typeof cm === 'function') {
-            closeMessage = cm(result);
-        } else if (cm === 'result') {
-            closeMessage = propertyKey + ' done. returns: ' + JSON.stringify(result);
-        } else if (cm === 'prettyResult') {
-            closeMessage = propertyKey + ' done. returns: \n' + JSON.stringify(result, null, ' ');
-        } else if (cm === 'className.functionName') {
-            closeMessage = fixedName + '.' + propertyKey + ' done';
-        }
+function getCloseMessage(
+  o: TraceOptions | undefined,
+  propertyKey: string,
+  result: any,
+  fixedName: string
+) {
+  const cm = o?.closeMessage;
+  let closeMessage = propertyKey + ' done';
+  if (is(cm)) {
+    if (typeof cm === 'function') {
+      closeMessage = cm(result);
+    } else if (cm === 'result') {
+      closeMessage = propertyKey + ' done. returns: ' + JSON.stringify(result);
+    } else if (cm === 'prettyResult') {
+      closeMessage =
+        propertyKey + ' done. returns: \n' + JSON.stringify(result, null, ' ');
+    } else if (cm === 'className.functionName') {
+      closeMessage = fixedName + '.' + propertyKey + ' done';
     }
-    return closeMessage;
+  }
+  return closeMessage;
 }
--- src/loxes/OutputLox.ts
+++ src/loxes/OutputLox.ts
@@ -4,69 +4,73 @@ import { Lox } from './Lox';
  * It extends the basic {@link Lox}.
  */
 export class OutputLox extends Lox {
-    /** the possibly sliced text of the logs corresponding module
-     * - is `''` if no module (`NONE`) or the default module (`DEFAULT`) was given
-     */
-    moduleText: string | '' = '';
-    /** the box layout of the log */
-    box: string | '' = '';
-    /** a string that represents the time consumption from the opening log's `timestamp` until this log appeared
-     * - is `''` when the log is a single `Loxer.log()` or an opening log itself
-     */
-    timeText: string | '' = '';
-    /** the time consumption (in `ms`) from the opening log's `timestamp` until this log appeared
-     * - is `undefined` when the log is a single `Loxer.log()` or an opening log itself
-     */
-    timeConsumption: number | undefined;
-    /**
-     * The colored versions of the log's `message`, `moduleText`, `box` and `timeText`
-     * - the coloring is done by wrapping the strings in {@link https://talyian.github.io/ansicolors/ ANSI Color Codes}
-     * - some consoles / platforms don't support the `\x1b[m` color codes. in that case it's reccommended to use the
-     *   uncolored strings
-     *
-     * You can easily switch between colored / uncolored versions like that:
-     * @example
-     * ```typescript
-     * const useColored = true; // switch here
-     * const { box, message, moduleText, timeText } = useColored ? outputLox.colored : outputlox;
-     * ```
-     * This is possible because the keys are named exactly the same. Loxer internally
-     */
-    colored: {
-        message: string;
-        moduleText: string | '';
-        box: string | '';
-        timeText: string | '';
+  /** the possibly sliced text of the logs corresponding module
+   * - is `''` if no module (`NONE`) or the default module (`DEFAULT`) was given
+   */
+  moduleText: string | '' = '';
+  /** the box layout of the log */
+  box: string | '' = '';
+  /** a string that represents the time consumption from the opening log's `timestamp` until this log appeared
+   * - is `''` when the log is a single `Loxer.log()` or an opening log itself
+   */
+  timeText: string | '' = '';
+  /** the time consumption (in `ms`) from the opening log's `timestamp` until this log appeared
+   * - is `undefined` when the log is a single `Loxer.log()` or an opening log itself
+   */
+  timeConsumption: number | undefined;
+  /**
+   * The colored versions of the log's `message`, `moduleText`, `box` and `timeText`
+   * - the coloring is done by wrapping the strings in {@link https://talyian.github.io/ansicolors/ ANSI Color Codes}
+   * - some consoles / platforms don't support the `\x1b[m` color codes. in that case it's reccommended to use the
+   *   uncolored strings
+   *
+   * You can easily switch between colored / uncolored versions like that:
+   * @example
+   * ```typescript
+   * const useColored = true; // switch here
+   * const { box, message, moduleText, timeText } = useColored ? outputLox.colored : outputlox;
+   * ```
+   * This is possible because the keys are named exactly the same. Loxer internally
+   */
+  colored: {
+    message: string;
+    moduleText: string | '';
+    box: string | '';
+    timeText: string | '';
+  };
+  /** determines if the log has not fulfilled the level that the corresponding module has set */
+  hidden: boolean = false;
+
+  constructor(prelog: Lox, coloredMessage: string) {
+    super(prelog);
+    this.colored = {
+      message: coloredMessage,
+      moduleText: '',
+      box: '',
+      timeText: '',
     };
-    /** determines if the log has not fulfilled the level that the corresponding module has set */
-    hidden: boolean = false;
+  }
 
-    constructor(prelog: Lox, coloredMessage: string) {
-        super(prelog);
-        this.colored = {
-            message: coloredMessage,
-            moduleText: '',
-            box: '',
-            timeText: '',
-        };
-    }
+  /** @internal */
+  setTime(times: {
+    timeText: string;
+    coloredTimeText: string;
+    timeConsumption?: number;
+  }) {
+    this.timeText = times.timeText;
+    this.colored.timeText = times.coloredTimeText;
+    this.timeConsumption = times.timeConsumption;
+  }
 
-    /** @internal */
-    setTime(times: { timeText: string; coloredTimeText: string; timeConsumption?: number }) {
-        this.timeText = times.timeText;
-        this.colored.timeText = times.coloredTimeText;
-        this.timeConsumption = times.timeConsumption;
-    }
+  /** @internal */
+  setModuleText(texts: { moduleText: string; coloredModuleText: string }) {
+    this.moduleText = texts.moduleText;
+    this.colored.moduleText = texts.coloredModuleText;
+  }
 
-    /** @internal */
-    setModuleText(texts: { moduleText: string; coloredModuleText: string }) {
-        this.moduleText = texts.moduleText;
-        this.colored.moduleText = texts.coloredModuleText;
-    }
-
-    /** @internal */
-    setBox(boxes: { box: string; cBox: string }) {
-        this.box = boxes.box;
-        this.colored.box = boxes.cBox;
-    }
+  /** @internal */
+  setBox(boxes: { box: string; cBox: string }) {
+    this.box = boxes.box;
+    this.colored.box = boxes.cBox;
+  }
 }
