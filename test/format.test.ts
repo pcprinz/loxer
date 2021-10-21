5db966d2a26bb83f04b48d5af67fbfb663217f2b
Christian Prinz <prinzach@b-tu.de>
2021-10-21 12:22:57 +0200

    added further tests

--- test/format.test.ts
+++ test/format.test.ts
@@ -1,4 +1,5 @@
-import { ANSIFormat } from '../src';
+import { ANSIFormat, Box, BoxFactory } from '../src';
+import { Loxes } from '../src/core/Loxes';
 import { ErrorLox } from '../src/loxes/ErrorLox';
 import { OutputLox } from '../src/loxes/OutputLox';
 
@@ -31,40 +32,15 @@ test('background coloring', () => {
 });
 
 test('lox coloring', () => {
-  const lox1 = new OutputLox({
-    highlighted: false,
-    id: 0,
-    level: 0,
-    message: 'Lox1!',
-    moduleId: 'Module',
-    type: 'close',
-    item: undefined,
-  });
-  lox1.color = '#fff';
-  lox1.moduleText = 'Module';
-  lox1.setTime({ timeText: '123', timeConsumption: 123 });
   const log1 = ANSIFormat.colorLox(lox1);
   expect(log1.message).toBe('\x1b[38;2;180;255;180mLox1!\x1b[0m');
   expect(log1.timeText).toBe('\x1b[38;2;70;70;70m123\x1b[0m');
   expect(log1.moduleText).toBe('\x1b[38;2;255;255;255mModule\x1b[0m');
-  const lox2 = new OutputLox({
-    highlighted: true,
-    id: 0,
-    level: 0,
-    message: 'Lox2!',
-    moduleId: 'Module',
-    type: 'single',
-    item: undefined,
-  });
-  lox2.color = '#fff';
-  lox2.moduleText = 'Module';
-  lox2.setTime({ timeText: '123', timeConsumption: 123 });
   const log2 = ANSIFormat.colorLox(lox2);
   expect(log2.message).toBe('\x1b[7mLox2!\x1b[0m');
   expect(log2.timeText).toBe('\x1b[38;2;70;70;70m123\x1b[0m');
   expect(log2.moduleText).toBe('\x1b[38;2;255;255;255mModule\x1b[0m');
-  const lox3 = new ErrorLox(lox1, new Error());
-  lox1.highlighted = true;
+  lox3.highlighted = true;
   lox3.color = '#fff';
   lox3.moduleText = 'Module';
   lox3.setTime({ timeText: '123', timeConsumption: 123 });
@@ -75,3 +51,65 @@ test('lox coloring', () => {
   expect(log3.timeText).toBe('\x1b[38;2;70;70;70m123\x1b[0m');
   expect(log3.moduleText).toBe('\x1b[38;2;153;153;153mModule\x1b[0m');
 });
+
+test('BoxLayout', () => {
+  const bl = new BoxFactory('round');
+  const loxes = new Loxes();
+  loxes.proceed(lox0);
+  lox1.hidden = true;
+  const box1 = bl.getLogBox(lox1, loxes);
+
+  bl.getOpenLogBox(lox0, loxes);
+  lox0.moduleId = 'INVALID';
+  bl.getOpenLogBox(lox0, loxes);
+  const boxx: Box = ['empty', { box: 'vertical', color: 'red' }];
+
+  const bs0 = bl.getBoxString(boxx, true);
+  const bs1 = bl.getBoxString(boxx, false);
+  const bs2 = bl.getBoxString(box1, false);
+  console.log('BOXES', [box1, boxx, bs1, bs2]);
+  expect(bs1).toBe(' │ ');
+  expect(bs2).toBe('');
+});
+
+// loxes
+const lox0 = new OutputLox({
+  highlighted: false,
+  id: 0,
+  level: 0,
+  message: 'Lox1!',
+  moduleId: 'Module',
+  type: 'open',
+  item: undefined,
+});
+lox0.color = '#fff';
+lox0.moduleText = 'Module';
+lox0.setTime({ timeText: '123', timeConsumption: 123 });
+
+const lox1 = new OutputLox({
+  highlighted: false,
+  id: 0,
+  level: 0,
+  message: 'Lox1!',
+  moduleId: 'Module',
+  type: 'close',
+  item: undefined,
+});
+lox1.color = '#fff';
+lox1.moduleText = 'Module';
+lox1.setTime({ timeText: '123', timeConsumption: 123 });
+
+const lox2 = new OutputLox({
+  highlighted: true,
+  id: 0,
+  level: 0,
+  message: 'Lox2!',
+  moduleId: 'Module',
+  type: 'single',
+  item: undefined,
+});
+lox2.color = '#fff';
+lox2.moduleText = 'Module';
+lox2.setTime({ timeText: '123', timeConsumption: 123 });
+
+const lox3 = new ErrorLox(lox1, new Error());
