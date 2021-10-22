e08ef93e9ce6cbd2dc17c805e32466edb9b9bd9a
Christian Prinz <prinzach@b-tu.de>
2021-10-22 16:43:59 +0200

    added further tests

--- test/decorators.test.ts
+++ test/decorators.test.ts
@@ -4,3 +4,38 @@ import { initLoxer, trace } from '../src';
 test('initLoxer', () => {
   initLoxer({});
 });
+class Test {
+  @trace('NONE') // @ts-ignore
+  async method() {
+    console.log('Worked');
+  }
+  @trace({
+    moduleId: 'NONE',
+    level: 1,
+    highlight: 'all',
+    argsAsItem: true,
+    openMessage: 'args',
+    closeMessage: 'result',
+    resultAsItem: true,
+  }) // @ts-ignore
+  method2() {
+    console.log('Worked');
+  }
+  @trace({ openMessage: 'functionName', closeMessage: 'functionName' }) // @ts-ignore
+  fn() {}
+  @trace({ openMessage: 'types', closeMessage: 'prettyResult' }) // @ts-ignore
+  types() {}
+  @trace({ openMessage: 'className.functionName', closeMessage: 'className.functionName' }) // @ts-ignore
+  classFn() {}
+}
+
+// TODO
+test('trace', async () => {
+  const a = new Test();
+  await a.method();
+
+  a.method2();
+  a.fn();
+  a.types();
+  a.classFn();
+});
